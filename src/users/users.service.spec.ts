f0dd00cff8e145aeb4e1efcf213b9d9dea9501c4
David <david.m.dias@gmail.com>
2021-05-01 16:54:39 +0100

    user login

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,38 @@
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { User, UsersSchema } from './users.schema';
+import { UsersService } from './users.service';
+import { closeInMongodConnection, rootMongooseTestModule } from '../test-utils/mongoose.test.module'
+
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{
+          name: User.name,
+          schema: UsersSchema,
+        }])
+      ],
+      providers: [UsersService],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return array from indexUsers', async () => {
+    const result = await service.indexUsers();
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
+  });
+});
