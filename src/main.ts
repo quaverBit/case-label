9c3ad5d1a603598e8e76a102421e2fb2d90d786c
David <david.m.dias@gmail.com>
2021-05-02 17:05:48 +0100

    migration comment

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+// Uncomment the following line to have migrations
 //import './migration.module';
 
 async function bootstrap() {
